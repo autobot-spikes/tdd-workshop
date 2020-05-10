2bb46f463dfb021440434cc4cc18642b01045729
Marc Lucchini <mlucchin@thoughtworks.com>
2020-05-10 12:51:07 +0700

    Exercise 1: possible solution

--- src/main/kotlin/com/ka/workshop/tdd/exercise1/FizzBuzz.kt
+++ src/main/kotlin/com/ka/workshop/tdd/exercise1/FizzBuzz.kt
@@ -1,7 +1,13 @@
 package com.ka.workshop.tdd.exercise1
 
 class FizzBuzz {
-    fun convert(): String {
-        return ""
+    private fun fizz(n: Int) = n % 3 == 0
+    private fun buzz(n: Int) = n % 5 == 0
+
+    fun convert(number: Int): String {
+        return if (fizz(number) && buzz(number)) "FizzBuzz"
+            else if (fizz(number)) "Fizz"
+            else if (buzz(number)) "Buzz"
+            else number.toString()
     }
 }
