dfd7ce040434696dd2d3d51b171370359f0e6210
Marc Lucchini <mlucchin@thoughtworks.com>
2020-05-10 12:51:07 +0700

    Exercise 1: possible solution

--- src/test/kotlin/com/ka/workshop/tdd/exercise1/FizzBuzzTest.kt
+++ src/test/kotlin/com/ka/workshop/tdd/exercise1/FizzBuzzTest.kt
@@ -1,14 +1,28 @@
 package com.ka.workshop.tdd.exercise1
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.data.blocking.forAll
+import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
 class FizzBuzzTest : FreeSpec({
-    "Replace this test" - {
-        val fizzBuzz = FizzBuzz()
+    "should return Fizz, Buzz, FizzBuzz or number if number is a multiple of 3, 5, both or none" - {
+        forAll(
+                row(3, "Fizz"),
+                row(6, "Fizz"),
+                row(5, "Buzz"),
+                row(10, "Buzz"),
+                row(15, "FizzBuzz"),
+                row(75, "FizzBuzz"),
+                row(2, "2"),
+                row(4, "4"),
+                row(7, "7")
+        ) { number, expected ->
+            val fizzBuzz = FizzBuzz()
 
-        val result = fizzBuzz.convert()
+            val result = fizzBuzz.convert(number)
 
-        result shouldBe ""
+            result shouldBe expected
+        }
     }
 })
