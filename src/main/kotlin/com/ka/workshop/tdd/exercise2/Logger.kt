e1205c8913c959c98db09f9864a63df29e052c00
Marc Lucchini <mlucchin@thoughtworks.com>
2020-05-11 16:38:37 +0700

    Exercise 2: Logger

---
+++ src/main/kotlin/com/ka/workshop/tdd/exercise2/Logger.kt
@@ -0,0 +1,7 @@
+package com.ka.workshop.tdd.exercise2
+
+class Logger {
+    fun log(message: String) {
+
+    }
+}
---
+++ src/test/kotlin/com/ka/workshop/tdd/exercise2/LoggerTest.kt
@@ -0,0 +1,11 @@
+package com.ka.workshop.tdd.exercise2
+
+import io.kotest.core.spec.style.FreeSpec
+
+class LoggerTest : FreeSpec({
+    "Replace this test" - {
+        val logger = Logger()
+
+        logger.log("")
+    }
+})
