6a7eae200bab980b1936af64e1c1803fe74d953c
Marc Lucchini <mlucchin@thoughtworks.com>
2020-05-11 17:42:05 +0700

    Exercise 2: possible solution

--- src/main/kotlin/com/ka/workshop/tdd/exercise2/Logger.kt
+++ src/main/kotlin/com/ka/workshop/tdd/exercise2/Logger.kt
@@ -1,7 +1,18 @@
 package com.ka.workshop.tdd.exercise2
 
-class Logger {
-    fun log(message: String) {
+import java.io.Writer
+import java.text.SimpleDateFormat
+import java.util.*
 
+class Logger(private val writer: Writer, private val calendar: Calendar) {
+    private val dateFormatter = SimpleDateFormat("dd-MM-yyyy hh:mm:ss")
+
+    init {
+        dateFormatter.timeZone = TimeZone.getTimeZone("GMT")
+    }
+
+    fun log(message: String) {
+        val now = dateFormatter.format(calendar.time)
+        writer.write("$now $message")
     }
 }
--- src/test/kotlin/com/ka/workshop/tdd/exercise2/LoggerTest.kt
+++ src/test/kotlin/com/ka/workshop/tdd/exercise2/LoggerTest.kt
@@ -1,11 +1,22 @@
 package com.ka.workshop.tdd.exercise2
 
 import io.kotest.core.spec.style.FreeSpec
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import java.io.Writer
+import java.util.*
 
 class LoggerTest : FreeSpec({
-    "Replace this test" - {
-        val logger = Logger()
+    "should log message with current formatted time" - {
+        val message = "my message"
+        val writer = mockk<Writer>(relaxed = true)
+        val calendar = mockk<Calendar>(relaxed = true)
+        val logger = Logger(writer, calendar)
+        every { calendar.time } returns Date(1589192448683)
 
-        logger.log("")
+        logger.log(message)
+
+        verify { writer.write("11-05-2020 10:20:48 $message") }
     }
 })
